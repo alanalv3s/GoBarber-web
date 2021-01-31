7a3d0b5d1496c6870816240f12269d7e08032122
alanalv3s <alanams97@gmail.com>
2021-01-30 22:52:59 -0300

    Animação dos Toasts

--- src/components/ToastContainer/Toast/styles.ts
+++ src/components/ToastContainer/Toast/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import { animated } from 'react-spring';
 
 interface ContainerProps {
   type?: 'success' | 'error' | 'info';
@@ -20,13 +21,13 @@ const toastTypeVariations = {
   `,
 };
 
-export const Container = styled.div<ContainerProps>`
+export const Container = styled(animated.div)<ContainerProps>`
   width: 360px;
 
   position: relative;
   padding: 16px 30px 16px 16px;
   border-radius: 10px;
-  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.3);
 
   display: flex;
 
